7fd14f455e5ee0a77d20948ea4429156fff2339b
Minh <incarnation1602@gmail.com>
2021-04-06 15:27:26 +0700

    Fix function output_mark

--- 1.student.mark.py
+++ 1.student.mark.py
@@ -1,3 +1,5 @@
+#! usr/bin/python3
+
 students = []
 students_name = []
 courses = []
@@ -28,7 +30,7 @@ def input_student_infor():
 
 def input_course_infor():
     for i in range(1, number_of_course+1):
-        input(f"Enter information of course #{i}:")
+        print(f"Enter information of course #{i}:")
         cid = int(input("Enter course id: "))
         cname = input("Enter course name: ")
         course = [cid, cname]
@@ -41,7 +43,7 @@ def input_mark():
     if cid in courses_id:
         for i in range(0, number_of_student):
             input_mark = input(f"Enter mark of course {cid} for student {students_name[i]} :")
-            mark_input = [courses_id[cid], students_name[i], input_mark]
+            mark_input = [cid, students_name[i], input_mark]
             marks.append(mark_input)
 
 def list_coures():
@@ -58,10 +60,9 @@ def output_mark():
     kk = int(input("Enter course id you want to display: "))
     if kk in courses_id:
         for each_item in marks:
-            if isinstance(each_item, list):
-               for nested_item in each_item:
-                   if nested_item == kk:
-                       print(each_item)
+            for nested_item in each_item:
+                if nested_item == kk:
+                    print(each_item)
 
 input_student_infor()
 input_course_infor()
