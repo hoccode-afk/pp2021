901507e54be019a00e1e9e189a617e98fe05ec3e
Minh <incarnation1602@gmail.com>
2021-05-13 02:15:17 +0700

    Deleted Compression.py and DecomLoad.py

--- PW6/Compression.py
+++
@@ -1,8 +0,0 @@
-from zipfile import ZipFile
-
-def compressing():
-    file_paths = ['students.txt', 'courses.txt', 'marks.txt']
-    with ZipFile('students.dat', 'w') as zip:
-        for file in file_paths:
-            zip.write(file)
-
